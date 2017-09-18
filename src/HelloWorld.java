0d8cd738699feb1db1a437e5a4218180d651116c
JohnnyB99 <s433561@ocsb.local>
2017-09-18 13:08:00 -0400

    First save

---
+++ src/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * This my hello world program.
+ * @author s433561
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//This is a single line comment
+		/*
+		 * This is 
+		 * a multiline
+		 * comment 
+		 */
+		System.out.println("Hello, I'm Johnny");	
+		System.out.print("This is my first program");
+		System.out.println("Isn't this fun??");
+		System.out.println("He said \"Do this now!\"");
+		
+	}
+}
